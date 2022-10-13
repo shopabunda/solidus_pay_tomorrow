50b72a339c854aad06dbba687af5d1f0d30aef4f
Abhishek <zenofabhishek@gmail.com>
2022-10-13 16:56:19 +0530

    Added HTML/JS that will call CreateOrderController
    1. When user will click on the Pay button on the payment step,
       the JS will do the post request to CreateOrderController,
       fetch the url and token and redirect the user to
       Paytomorrow's URL
    2. We disable the Pay button when clicked once so that,
       we can avoid duplicate requests
    3. We show a very basic success/failure message on Frontend
       based on CreateOrderController's response

---
+++ app/assets/javascripts/solidus_pay_tomorrow.js
@@ -0,0 +1,58 @@
+let orderNumber;
+let paymentMethodId;
+let orderToken;
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const createOrderPayload = document.getElementById(
+        'pay-tomorrow-order-payload'
+    );
+    if (createOrderPayload){
+        orderNumber = createOrderPayload.dataset.orderNumber;
+        paymentMethodId = createOrderPayload.dataset.paymentMethodId
+        orderToken = createOrderPayload.dataset.orderToken;
+    }
+})
+
+async function createOrder() {
+    const createOrderResponse = await createPayTomorrowOrder();
+    const createOrderStatusDiv = document.getElementById('create-order-status-container');
+    if (createOrderResponse.ok) {
+        document.getElementById("card-container").remove()
+        document.getElementById("pay-tomorrow-card-button").remove()
+        createOrderStatusDiv.innerHTML = "URL generated. Redirecting to payTomorrow to complete the payment..."
+    } else {
+        createOrderStatusDiv.innerHTML = "Payment Failed"
+    }
+    return createOrderResponse.json();
+}
+
+async function createPayTomorrowOrder() {
+    const body = JSON.stringify({
+        payment_method: paymentMethodId
+    });
+    const resp = await fetch('/api/orders/' + orderNumber + '/pay_tomorrow', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Spree-Order-Token': orderToken
+        },
+        body,
+    });
+    return resp
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const cardButton = document.getElementById(
+        'pay-tomorrow-card-button'
+    );
+    cardButton.addEventListener('click', async function () {
+        cardButton.disabled = true;
+        const response = await createOrder();
+        if (response.hasOwnProperty('url')) {
+            window.open(response.url, '_blank')
+        } else {
+            cardButton.disabled = false;
+        }
+    });
+});
+
