76fe3658864cd7f6dbe59857b821f70aa6c2bdfd
Abhishek <zenofabhishek@gmail.com>
2022-11-10 16:58:28 +0530

    Handle BE specific code
    1. Added the partial that'll show that PayTomorrow payments
       aren't available on BE.
       This is because the redirect on OrderApplicationController is done
       by PayTomorrow and we don't have info on order. We'll find a clean
       way to handle this later. Until then, we'll show
       "PayTomorrow payments are only available on FE right now"
    2. Removed the spree/frontend/solidus_pay_tomorrow from
       install_generator

--- lib/generators/solidus_pay_tomorrow/install/install_generator.rb
+++ lib/generators/solidus_pay_tomorrow/install/install_generator.rb
@@ -17,7 +17,6 @@ module SolidusPayTomorrow
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js',
           "//= require spree/frontend/solidus_pay_tomorrow\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_pay_tomorrow\n"
       end
 
       def add_stylesheets
