4a651da3f1b105b2b4adcd6a47b0f9f160884e2d
Abhishek <zenofabhishek@gmail.com>
2022-11-10 17:54:01 +0530

    Make the pay_tomorrow seed rake task idempotent
    Did this to avoid creating multiple payment methods
    if we run install generator multiple times

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-if ENV['PAY_TOMORROW_USERNAME'] && ENV['PAY_TOMORROW_PASSWORD'] && ENV['PAY_TOMORROW_SIGNATURE']
+if ENV['PAY_TOMORROW_USERNAME'] && ENV['PAY_TOMORROW_PASSWORD'] && ENV['PAY_TOMORROW_SIGNATURE'] &&
+  SolidusPayTomorrow::PaymentMethod.count == 0
   SolidusPayTomorrow::PaymentMethod.create!(
     type: 'SolidusPayTomorrow::PaymentMethod',
     name: 'PayTomorrow',
