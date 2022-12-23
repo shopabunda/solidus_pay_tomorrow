523e5b1cf97b0ad96dff33c425a0090b963f8105
Abhishek <zenofabhishek@gmail.com>
2022-12-23 21:06:54 +0530

    Use SuccessHandler in OrderApplicationController
    OrderApplicationController is responsible for handling
    the PayTomorrow redirects.
    Now that we have an idempotent method for handling PayTomorrow
    completed applications, it's crucial we use SuccessHandler
    to avoid the condition where an order is being updated by
    both the redirect and the notify callback

--- lib/generators/solidus_pay_tomorrow/install/templates/solidus_pay_tomorrow_order_application_controller.rb
+++ lib/generators/solidus_pay_tomorrow/install/templates/solidus_pay_tomorrow_order_application_controller.rb
@@ -3,20 +3,16 @@
 module SolidusPayTomorrow
   class OrderApplicationController < Spree::StoreController
     def success
-      # If auto_capture is disabled, the payment is already processed and
-      # it'll be captured from admin. Marking as pending is necessary to avoid
-      # calling authorize! in this case
-      payment.update!(state: :pending) unless payment.payment_method.auto_capture?
-      current_order.next!
+      SolidusPayTomorrow::Handlers::SuccessHandler.call(current_order, payment)
 
-      flash[:notice] = 'Payment Successful!'
+      flash[:notice] = 'PayTomorrow Application Successful!'
       redirect_to checkout_state_path('confirm')
     end
 
     def cancel
       payment.update!(state: :invalid)
 
-      flash[:error] = "Payment failed!"
+      flash[:error] = "PayTomorrow Application failed!"
       redirect_to checkout_state_path('payment')
     end
 
